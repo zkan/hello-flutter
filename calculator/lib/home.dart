2bbe35a205d6383fbde6963d9e0ef0f2f621e04d
Kan Ouivirach <zkan.cs@gmail.com>
2023-03-09 16:51:29 +0700

    feat: Add button examples

--- calculator/lib/home.dart
+++ calculator/lib/home.dart
@@ -8,34 +8,50 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: appBar(),
       body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            height: 200,
-            width: 150,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.pink.shade200,
-              borderRadius: const BorderRadius.only(
-                bottomRight: Radius.circular(24),
-              )
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 200,
+              width: 150,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.pink.shade200,
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(24),
+                  )),
+              child: const Text("Calculator Left"),
             ),
-            child: const Text("Calculator Left"),
-          ),
-          Container(
-            height: 200,
-            width: 150,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.pink.shade200,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(24),
-              )
+            Container(
+              height: 200,
+              width: 150,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.pink.shade200,
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(24),
+                  )),
+              child: const Text("Calculator Right"),
             ),
-            child: const Text("Calculator Right"),
-          ),
-        ],
-      ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {},
+                        child: Text("0"),
+                        style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                          fixedSize: Size(54, 54),
+                        )),
+                    OutlinedButton(onPressed: () {}, child: Text("0")),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                    TextButton(onPressed: () {}, child: Text("0")),
+                  ],
+                ),
+              ],
+            ),
+          ],
+      )
     );
   }
 
