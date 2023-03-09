250bb4e698fe4227bd65df587bab33b1ed217beb
Kan Ouivirach <zkan.cs@gmail.com>
2023-03-09 16:02:30 +0700

    refactor: Extract to method

--- calculator/lib/home.dart
+++ calculator/lib/home.dart
@@ -6,26 +6,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "ODDS Calculator",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-            height: 1.5,
-            color: Colors.indigo.shade800,
-          ),
-          textAlign: TextAlign.left,
-          maxLines: 4,
-          overflow: TextOverflow.ellipsis,
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        "ODDS Calculator",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+          height: 1.5,
+          color: Colors.indigo.shade800,
         ),
-        elevation: 10,
-        actions: [
-          Icon(Icons.notifications),
-          Icon(Icons.person),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
-        ],
+        textAlign: TextAlign.left,
+        maxLines: 4,
+        overflow: TextOverflow.ellipsis,
       ),
+      elevation: 10,
+      actions: [
+        Icon(Icons.notifications),
+        Icon(Icons.person),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.person))
+      ],
     );
   }
 }
